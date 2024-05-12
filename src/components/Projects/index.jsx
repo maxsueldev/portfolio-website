024f8d962c9e825c9d63791b18fe57a67ad298a9
unknown <{ID}+{username}@users.noreply.github.com>
2024-05-11 22:43:57 -0300

    portfolio project

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -14,7 +14,7 @@ function Projects() {
     const [currentPage, setCurrentPage] = useState(1);
     const [projectsPerPage] = useState(4);
 
-    const apiProjects = 'https://json-server-projects.vercel.app/projects';
+    const apiProjects = 'https://portfolio-maxsuel.vercel.app/services/projects';
 
     useEffect(() => {
         const getDataProjects = async () => {
