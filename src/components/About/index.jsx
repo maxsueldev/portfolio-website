1dd5c0f53e547f86fe8116a322cdc1d39008d955
Maxsuel Santos <maxsuel.dev@gmail.com>
2024-12-05 21:55:51 -0300

    Update index.jsx

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,7 +6,7 @@ function About() {
             <h1>Sobre mim</h1>
             <p>Olá, me chamo Maxsuel Fernando e desde pequeno tenho grande interesse pelo mundo da tecnologia, sempre dizendo aos meus pais
                 que queria trabalhar com informática. Durante minha jornada acadêmica me via perdido com o tanto de ramos que a
-                tecnologia abrange e mesmo querendo ser programador, não tinha noção de qual linguagem estudar ou carrereira seguir. Só em 2017 percebi
+                tecnologia abrange e mesmo querendo ser programador, não tinha noção de qual linguagem estudar ou carreira seguir. Só em 2017 percebi
                 que o Front-end é minha área de interesse e venho estudando e me dedicando cada vez mais.</p>
         </AboutContainer>
     );
