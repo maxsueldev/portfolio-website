dc0855293a4d15e1be8c4f12000e33b9a6ad721c
unknown <{ID}+{username}@users.noreply.github.com>
2025-09-29 21:16:42 -0300

    data-aos

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
   useEffect(() => {
     AOS.init({
       offset: 120, // offset (in px) from the original trigger point
-      delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 400, // values from 0 to 3000, with step 50ms
+      delay: 200, // values from 0 to 3000, with step 50ms
+      duration: 800, // values from 0 to 3000, with step 50ms
       easing: "ease", // default easing for AOS animations
       anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
     });
--- src/components/Experiences/Experience/index.tsx
+++ src/components/Experiences/Experience/index.tsx
@@ -12,10 +12,19 @@ function Experience({
   local,
   description = "",
   date,
-  ...rest
 }: IExperienceProps) {
   return (
-    <div className={styles.experienceContainer} {...rest}>
+    <div
+      className={styles.experienceContainer}
+      data-aos="fade-up"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center"
+    >
       <div className={styles.content}>
         <p className={styles.title}>{title}</p>
         <p className={styles.local}>{local}</p>
