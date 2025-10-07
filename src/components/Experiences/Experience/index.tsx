18a12d8312515c08d80f0a9c2d388aff529253d6
unknown <{ID}+{username}@users.noreply.github.com>
2025-10-07 20:34:31 -0300

    update portfolio

--- src/components/Experiences/Experience/index.tsx
+++ src/components/Experiences/Experience/index.tsx
@@ -12,12 +12,7 @@ const Experience = ({ title, local, description = "", date }: IExperience) => {
       }`}
       data-aos="fade-down"
       data-aos-offset="200"
-      data-aos-delay="50"
-      data-aos-duration="1000"
-      data-aos-easing="ease-in-out"
       data-aos-mirror="true"
-      data-aos-once="false"
-      data-aos-anchor-placement="top-center"
     >
       <div className={styles.content}>
         <p className={styles.title}>{title}</p>
